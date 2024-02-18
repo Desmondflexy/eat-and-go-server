98cc3c98d922b25970d46c2a9de78e8442092c9b
Desmondflexy <ikilodesmond@gmail.com>
2024-02-18 12:32:52 +0100

    add cors

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,26 +3,26 @@ export const baseUrl =
     ? `http://localhost:${process.env.PORT}`
     : process.env.SERVER_URL;
 
-/** Generates an HTML email verification message using the user's name and token for a signup with Eat-and-Go. */
-export const getEmailVerificationText = (name: string, token: string) => {
+/** Generates an HTML email verification message using the user's name and id for a signup with Eat-and-Go. */
+export const getEmailVerificationText = (name: string, userId: string) => {
   const message = `
   <p>Hi ${name},</p>
   <p>Thank you for signing up with Eat-and-Go</p>
   <p>Click the button to verify your email address</p> <br>
-  <a href="${baseUrl}/auth/email-verify/${token}">
+  <a href="${baseUrl}/auth/email-verify/${userId}">
     <button style="background-color: blue; border-radius: 4px; border: none; color: white; padding: 8px 12px;">
       Verify Email
     </button>
   </a>
-  `;
+  `;˚∑
   return message;
 };
 
-export const getResetPasswordText = (name: string, token: string) => {
+export const getResetPasswordText = (name: string, userId: string) => {
   const message = `
   <p>Hi ${name},</p>
   <p>Click the button to reset your password</p> <br>
-  <a href="${process.env.CLIENT_URL}/reset-password/${token}">
+  <a href="${process.env.CLIENT_URL}/reset-password/${userId}">
     <button style="background-color: blue; border-radius: 4px; border: none; color: white; padding: 8px 12px;">
       Reset Password
     </button>
